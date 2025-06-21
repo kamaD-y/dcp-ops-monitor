f7a88445896e45826a78e31b8a0a0af3322cdd4a
kamaD-y <yk28japaaaan@gmail.com>
2025-06-21 18:19:01 +0900

    test(etl): テストがパスするよう修正

--- lambda/etl/tests/domain/conftest.py
+++ lambda/etl/tests/domain/conftest.py
@@ -2,24 +2,6 @@ from datetime import datetime, timedelta
 
 import pytest
 
-@pytest.fixture
-def valid_assets_info():
-    """テスト用のDcpAssetsInfoオブジェクトを生成する"""
-    from src.domain.dcp_value_object import DcpAssetsInfo, DcpProductAssets, DcpTotalAssets
-    return DcpAssetsInfo(
-        total=DcpTotalAssets(
-            cumulative_contributions="900,000円", total_gains_or_losses="300,000円", total_asset_valuation="1,200,000円"
-        ),
-        products={
-            "プロダクト_1": DcpProductAssets(
-                cumulative_acquisition_costs="100,000円", gains_or_losses="11,111円", asset_valuation="111,111円"
-            ),
-            "プロダクト_2": DcpProductAssets(
-                cumulative_acquisition_costs="200,000円", gains_or_losses="22,222円", asset_valuation="222,222円"
-            ),
-        },
-    )
-
 
 @pytest.fixture
 def valid_ops_indicators():
