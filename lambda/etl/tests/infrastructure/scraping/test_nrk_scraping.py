e12d3d6b42cdd7d433181fe891cea352dcd03232
kamaD-y <yk28japaaaan@gmail.com>
2025-06-23 09:50:14 +0900

    test(etl): extraction構成リファクタリングに伴うテスト修正

--- lambda/etl/tests/infrastructure/scraping/test_nrk_scraping.py
+++ lambda/etl/tests/infrastructure/scraping/test_nrk_scraping.py
@@ -1,4 +1,7 @@
 import os
+import pytest
+
+from bs4.element import Tag
 
 
 def test_scrape__get_parameter_called_when_login_parameter_arn_set(mocker) -> None:
@@ -34,3 +37,63 @@ def test_scrape__get_parameter_not_called_when_login_parameter_arn_not_set(mocke
 
     # then
     mock_get_parameter.assert_not_called()
+
+
+def test_extract__valid_assets_page(valid_assets_page: str, mocker) -> None:
+    from src.infrastructure.scraping.nrk_scraping import NRKScraper
+    # given
+    scraper = NRKScraper(user_id="test", password="test", birthdate="20200101", driver=mocker.Mock())
+    scraper.page_source = valid_assets_page
+
+
+    # when
+    assets_info = scraper.extract()
+
+    # then
+    assert assets_info.total.cumulative_contributions == "900,000円"
+    assert assets_info.total.total_gains_or_losses == "300,000円"
+    assert assets_info.total.total_asset_valuation == "1,200,000円"
+
+    assert assets_info.products["プロダクト_1"].cumulative_acquisition_costs == "100,000円"
+    assert assets_info.products["プロダクト_1"].gains_or_losses == "11,111円"
+    assert assets_info.products["プロダクト_1"].asset_valuation == "111,111円"
+
+    assert assets_info.products["プロダクト_2"].cumulative_acquisition_costs == "200,000円"
+    assert assets_info.products["プロダクト_2"].gains_or_losses == "22,222円"
+    assert assets_info.products["プロダクト_2"].asset_valuation == "222,222円"
+
+
+def test_extract__invalid_assets_page(invalid_assets_page: str, mocker) -> None:
+    from src.infrastructure.scraping.nrk_scraping import NRKScraper, ExtractError
+    # given
+    scraper = NRKScraper(user_id="test", password="test", birthdate="20200101", driver=mocker.Mock())
+    scraper.page_source = invalid_assets_page
+
+    # when, then
+    with pytest.raises(ExtractError):
+        scraper.extract()
+
+
+def test_is_tag_element__tag(mocker) -> None:
+    from src.infrastructure.scraping.nrk_scraping import NRKScraper
+    # given
+    scraper = NRKScraper(user_id="test", password="test", birthdate="20200101", driver=mocker.Mock())
+
+    # when
+    tag = Tag(name="div")
+
+    # then
+    assert scraper._is_tag_element(tag) is True
+
+
+def test_is_tag_element__not_tag(mocker) -> None:
+    from src.infrastructure.scraping.nrk_scraping import NRKScraper
+    # given
+    scraper = NRKScraper(user_id="test", password="test", birthdate="20200101", driver=mocker.Mock())
+
+    # when
+    not_tag = "not a tag"
+
+    # then
+    assert scraper._is_tag_element(not_tag) is False
+
