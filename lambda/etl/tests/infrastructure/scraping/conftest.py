4ef2cb7857cc9b44faba5479637e48cbc87d45f4
kamaD-y <yk28japaaaan@gmail.com>
2025-06-25 08:35:24 +0900

    test(etl): scraping用conftest改善

--- lambda/etl/tests/infrastructure/scraping/conftest.py
+++ lambda/etl/tests/infrastructure/scraping/conftest.py
@@ -1,12 +1,11 @@
-from datetime import datetime, timedelta
-
+from pathlib import Path
 import pytest
 
 
 @pytest.fixture
 def valid_assets_page() -> str:
     """テスト用の正常なHTMLファイルを読み込む"""
-    with open("tests/fixtures/html/valid_assets_page.html") as f:
+    with open(Path(__file__).parents[2] / "fixtures/html/valid_assets_page.html") as f:
         assets_page = f.read()
     return assets_page
 
@@ -14,6 +13,6 @@ def valid_assets_page() -> str:
 @pytest.fixture
 def invalid_assets_page() -> str:
     """テスト用の不正なHTMLファイルを読み込む"""
-    with open("tests/fixtures/html/invalid_assets_page.html") as f:
+    with open(Path(__file__).parents[2] / "fixtures/html/invalid_assets_page.html") as f:
         assets_page = f.read()
     return assets_page
