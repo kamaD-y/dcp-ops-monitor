c3cbc5daa9d71e04d6180ab2a8e7267c47bd83a3
kamaD-y <yk28japaaaan@gmail.com>
2025-07-04 14:00:55 +0900

    test: docker-composeでLocalStackを使用する

---
+++ localstack/ready.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 起動時にバケットを作成する
+awslocal s3 mb s3://${ERROR_BUCKET_NAME}
+
+# 起動時にParameterStoreにパラメータを登録する
+awslocal ssm put-parameter --name ${LOGIN_PARAMETER_NAME} --value ${LOGIN_PARAMETER_VALUE} --type SecureString --overwrite
+
+# 起動時にSNSトピックを作成する
+awslocal sns create-topic --name ${SNS_TOPIC_NAME}
