1d42265d2b70c003f5853d2fa90dcc130372a05f
kamaD-y <yk28japaaaan@gmail.com>
2025-12-20 21:14:02 +0900

    improve: パラメータストア取得をboto3使用に変更

--- lambda/web-scraping/src/infrastructure/ssm_parameter.py
+++ lambda/web-scraping/src/infrastructure/ssm_parameter.py
@@ -1,25 +1,21 @@
-from aws_lambda_powertools.utilities import parameters
+import json
+import os
+from typing import Any, Dict
+
+import boto3
+
+if os.environ.get("ENV") == "test":
+    client = boto3.client("ssm", region_name="ap-northeast-1", endpoint_url=os.environ["LOCAL_STACK_CONTAINER_URL"])
+else:
+    client = boto3.client("ssm", region_name="ap-northeast-1")
 
 
-def get_ssm_json_parameter(name: str, decrypt: bool = True) -> dict:
-    """SSMパラメータストアからJSON形式のパラメータを取得する
-    Args:
-        name (str): パラメータ名
-        decrypt (bool, optional): 暗号化されたパラメータを復号化するかどうか. Defaults to True.
-    Returns:
-        dict: 取得したパラメータ値
-    """
-    return parameters.get_parameter(name=name, decrypt=decrypt, transform="json")
+def get_ssm_json_parameter(name: str, decrypt: bool = True) -> Dict[str, Any]:
+    response = client.get_parameter(Name=name, WithDecryption=decrypt)
+    parameters_json = response["Parameter"]["Value"]
+    return json.loads(parameters_json)
 
 
 def get_ssm_parameter(name: str, decrypt: bool = True) -> str:
-    """SSMパラメータストアからパラメータを取得する
-
-    Args:
-        name (str): パラメータ名
-        decrypt (bool, optional): 暗号化されたパラメータを復号化するかどうか. Defaults to True.
-
-    Returns:
-        str: 取得したパラメータ値
-    """
-    return parameters.get_parameter(name=name, decrypt=decrypt)
+    response = client.get_parameter(Name=name, WithDecryption=decrypt)
+    return response["Parameter"]["Value"]
