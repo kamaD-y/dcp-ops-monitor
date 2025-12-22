c1c12ee2c9f226de5593d4dffb7e33385000587e
kamaD-y <yk28japaaaan@gmail.com>
2025-12-22 10:21:53 +0900

    fix: 通知文が円表記されるよう修正

--- lambda/web-scraping/src/infrastructure/line_notifier.py
+++ lambda/web-scraping/src/infrastructure/line_notifier.py
@@ -36,9 +36,9 @@ class LineNotifier(INotifier):
     def create_message_by(self, assets_info: DcpAssets, ops_indicators: DcpOpsIndicators) -> str:
         message = "確定拠出年金 運用状況通知Bot\n\n"
         message += "総評価\n"
-        message += f"拠出金額累計: {assets_info.total.cumulative_contributions}\n"
-        message += f"評価損益: {assets_info.total.gains_or_losses}\n"
-        message += f"資産評価額: {assets_info.total.asset_valuation}\n"
+        message += f"拠出金額累計: {assets_info.total.cumulative_contributions:,.0f}円\n"
+        message += f"評価損益: {assets_info.total.gains_or_losses:,.0f}円\n"
+        message += f"資産評価額: {assets_info.total.asset_valuation:,.0f}円\n"
         message += "\n"
         message += f"運用年数: {ops_indicators.operation_years}年\n"
         message += f"運用利回り: {ops_indicators.actual_yield_rate}\n"
@@ -49,9 +49,9 @@ class LineNotifier(INotifier):
         message += "商品別\n"
         for p_name, p_v in assets_info.products.items():
             message += f"{p_name}\n"
-            message += f"取得価額累計: {p_v.cumulative_contributions}\n"
-            message += f"損益: {p_v.gains_or_losses}\n"
-            message += f"資産評価額: {p_v.asset_valuation}\n"
+            message += f"取得価額累計: {p_v.cumulative_contributions:,.0f}円\n"
+            message += f"損益: {p_v.gains_or_losses:,.0f}円\n"
+            message += f"資産評価額: {p_v.asset_valuation:,.0f}円\n"
             message += "\n"
 
         return message
